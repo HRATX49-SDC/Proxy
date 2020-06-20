b040b5d386b210304093cbbbbaa2e760defcfccb
Anthony Casper <tonycasper21@gmail.com>
2020-06-20 16:50:18 -0500

    Implements proxy server using AWS S3

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const http = require('http');
 const express = require('express');
+const path = require('path')
 const httpProxy = require('http-proxy');
-const expressProxy = require('express-http-proxy');
 const PORT = 5555;
+
 const app = express();
-
-// app.use(expressProxy('https://localhost:5000'))
-
-// app.listen(5555, () => {
-//   console.log(`Express Proxy is listening on port ${PORT}`)
-// })
-
 var proxy = httpProxy.createProxyServer({});
 
-http.createServer(function(req,res) {
-  proxy.web(req, res, {target: 'http://localhost:5000'});
-}).listen(5555);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-console.log("Proxy running on port 5555")
+app.get('/main', (req, res) => {
+  console.log('redirecting to mainImage server')
+  proxy.web(req, res, {target: `http://localhost:5000/`});
+});
+
+app.get('/purrget', (req,res) => {
+  console.log('redirecting to aboutItem server');
+  proxy.web(req, res, {target: `http://localhost:5100/`});
+})
+
+app.listen(PORT, () => {
+  console.log(`Express Proxy is listening on port ${PORT}`);
+});
