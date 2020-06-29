921964967e142c052369d799beb33c3848590e47
Anthony Casper <tonycasper21@gmail.com>
2020-06-29 16:29:09 -0500

    Updated Proxy with 4 services combined

--- index.compiled.js
+++ index.compiled.js
@@ -15,16 +15,28 @@ app.use(express.urlencoded({
 }));
 app.use(express["static"](path.join(__dirname, 'public'))); //routes
 
-app.get('/reccomended*', function (req, res) {
+app.all('/reccomended*', function (req, res) {
   console.log('Redirecting to reccomended service');
   proxy.web(req, res, {
     target: 'http://reccomended-featuredserver-dev.us-east-2.elasticbeanstalk.com/'
   });
 });
-app.get('/main', function (req, res) {
+app.all('/main', function (req, res) {
   console.log('redirecting to mainImage server');
   proxy.web(req, res, {
     target: "http://purrgetmainitemdisplay-env.eba-upicdvwk.us-east-2.elasticbeanstalk.com/"
+  });
+});
+app.all('/about', function (req, res) {
+  console.log('redirecting to about server');
+  proxy.web(req, res, {
+    target: "http://purrgetaboutthisitem-dev.us-east-1.elasticbeanstalk.com/"
+  });
+});
+app.all('/search', function (req, res) {
+  console.log('redirecting to search server');
+  proxy.web(req, res, {
+    target: "http://v50-dev.us-east-1.elasticbeanstalk.com/"
   });
 }); //listen
 
--- index.js
+++ index.js
@@ -11,14 +11,24 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //routes
-app.get('/reccomended*', (req, res) => {
+app.all('/reccomended*', (req, res) => {
   console.log('Redirecting to reccomended service')
   proxy.web(req, res, {target: 'http://reccomended-featuredserver-dev.us-east-2.elasticbeanstalk.com/'})
 })
 
-app.get('/main', (req, res) => {
+app.all('/main', (req, res) => {
   console.log('redirecting to mainImage server')
   proxy.web(req, res, {target: `http://purrgetmainitemdisplay-env.eba-upicdvwk.us-east-2.elasticbeanstalk.com/`});
+});
+
+app.all('/about', (req, res) => {
+  console.log('redirecting to about server')
+  proxy.web(req, res, {target: `http://purrgetaboutthisitem-dev.us-east-1.elasticbeanstalk.com/`});
+});
+
+app.all('/search', (req, res) => {
+  console.log('redirecting to search server')
+  proxy.web(req, res, {target: `http://v50-dev.us-east-1.elasticbeanstalk.com/`});
 });
 
 //listen
