125eb2aa84bfaf3b4cf158db2b5a74263f0467e4
Anthony Casper <tonycasper21@gmail.com>
2020-07-02 15:14:44 -0500

    Adds some speed optimization

--- index.compiled.js
+++ index.compiled.js
@@ -4,11 +4,14 @@ var express = require('express');
 
 var path = require('path');
 
+var compression = require('compression');
+
 var httpProxy = require('http-proxy');
 
 var PORT = process.env.PORT || 5002;
 var app = express();
 var proxy = httpProxy.createProxyServer({});
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
@@ -16,27 +19,28 @@ app.use(express.urlencoded({
 app.use(express["static"](path.join(__dirname, 'public'))); //routes
 
 app.all('/reccomended*', function (req, res) {
-  console.log('Redirecting to reccomended service');
   proxy.web(req, res, {
     target: 'http://reccomended-featuredserver-dev.us-east-2.elasticbeanstalk.com/'
   });
 });
-app.all('/main', function (req, res) {
-  console.log('redirecting to mainImage server');
+app.all('/main*', function (req, res) {
   proxy.web(req, res, {
-    target: "http://purrgetmainitemdisplay-env.eba-upicdvwk.us-east-2.elasticbeanstalk.com/"
+    target: "http://purrgetmainitemv2.us-east-2.elasticbeanstalk.com/"
   });
 });
 app.all('/about*', function (req, res) {
-  console.log('redirecting to about server');
   proxy.web(req, res, {
     target: "http://purrgetaboutthisitem-dev.us-east-1.elasticbeanstalk.com/"
   });
 });
 app.all('/search*', function (req, res) {
-  console.log('redirecting to search server');
   proxy.web(req, res, {
     target: "http://v50-dev.us-east-1.elasticbeanstalk.com/"
+  });
+});
+app.all('/reviews*', function (req, res) {
+  proxy.web(req, res, {
+    target: "http://service-dev2.us-west-2.elasticbeanstalk.com/"
   });
 }); //listen
 
--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 const httpProxy = require('http-proxy');
 const PORT = process.env.PORT || 5002;
 
 const app = express();
 const proxy = httpProxy.createProxyServer({});
 
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //routes
 app.all('/reccomended*', (req, res) => {
-  console.log('Redirecting to reccomended service')
   proxy.web(req, res, {target: 'http://reccomended-featuredserver-dev.us-east-2.elasticbeanstalk.com/'})
 })
 
-app.all('/main', (req, res) => {
-  console.log('redirecting to mainImage server')
+app.all('/main*', (req, res) => {
   proxy.web(req, res, {target: `http://purrgetmainitemv2.us-east-2.elasticbeanstalk.com/`});
 });
 
 app.all('/about*', (req, res) => {
-  console.log('redirecting to about server')
   proxy.web(req, res, {target: `http://purrgetaboutthisitem-dev.us-east-1.elasticbeanstalk.com/`});
 });
 
 app.all('/search*', (req, res) => {
-  console.log('redirecting to search server')
   proxy.web(req, res, {target: `http://v50-dev.us-east-1.elasticbeanstalk.com/`});
 });
+
+app.all('/reviews*', (req, res) => {
+  proxy.web(req, res, {target: `http://service-dev2.us-west-2.elasticbeanstalk.com/`});
+})
 
 //listen
 app.listen(PORT, () => {
